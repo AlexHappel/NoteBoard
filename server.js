f9ef30373bfa6a89016c76ce23ed23b9dee827bb
Alexander Happel <Alex.happel90@gmail.com>
2024-06-24 16:47:49 -0400

    refactored server.js

--- server.js
+++ server.js
@@ -10,47 +10,47 @@ app.use(express.static('public'));
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
-});
+    });
 });
 
 app.post('/api/notes', (req, res) => {
-const newNote = req.body;
-fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    const newNote = req.body;
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     notes.push(newNote);
     fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-    if (err) throw err;
-    res.json(newNote);
+        if (err) throw err;
+        res.json(newNote);
     });
-});
+    });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-const noteId = req.params.id;
-fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    const noteId = req.params.id;
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     const filteredNotes = notes.filter(note => note.id !== noteId);
     fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(filteredNotes, null, 2), (err) => {
-    if (err) throw err;
-    res.json({ success: true });
+        if (err) throw err;
+        res.json({ success: true });
     });
-});
+    });
 });
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
-console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
